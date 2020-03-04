00741558511b5f8758f185f3b1d77d0cc61fc835
irene-haeryun-kwon <haeryun2004@gmail.com>
2020-03-04 16:44:17 -0500

    Added calendar using react-native-calendars

---
+++ assets/styles/Trip.js
@@ -0,0 +1,18 @@
+import * as Common from './Common';
+
+export default {
+    calendar: {
+        paddingVertical: 20
+    },
+    calendarTheme: {
+        selectedDayBackgroundColor: Common.BLUE,
+        todayTextColor: Common.BLUE,
+        arrowColor: Common.BLUE,
+        textDayFontFamily: Common.REGULAR_FONT_STYLE,
+        textMonthFontFamily: Common.SEMI_FONT_STYLE,
+        textDayHeaderFontFamily: Common.REGULAR_FONT_STYLE,
+        textDayFontSize: Common.SMALL_FONT_SIZE,
+        textMonthFontSize: Common.MID_FONT_SIZE,
+        textDayHeaderFontSize: Common.SMALL_FONT_SIZE,
+    }
+}
--- screens/Trip.js
+++ screens/Trip.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import moment from 'moment';
+
+import Theme from '../assets/styles/Theme';
+import Font from '../assets/styles/Font';
+import Styles from '../assets/styles/Trip';
 
 class Trip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        selected: moment(new Date()).format('YYYY-MM-DD')
+    };
+    this.onDayPress = this.onDayPress.bind(this);
+  }
+
+  onDayPress(day) {
+      this.setState({
+          selected: day.dateString
+      });
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text>Trip Page</Text>
+      <View>
+        <Font
+          size='xl'
+          text='trip'
+          weight='semi'
+          transform='cap'
+        />
+        <Calendar
+          style={Styles.calendar}
+          theme={Styles.calendarTheme}
+          monthFormat={'MMM yyyy'}
+          markingType={'multi-dot'}
+          markedDates={{
+            [this.state.selected]: {selected: true, disableTouchEvent: true},
+          }}
+          onDayPress={this.onDayPress}
+        />
       </View>
     );
   }
