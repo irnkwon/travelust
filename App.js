d0ab8f3940ead6c62fb282b72564ea8ff9f064a0
Fahad <Fahad@LAPTOP-V72H65G9>
2020-03-05 15:32:25 -0500

    Added basics for search functionality

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Intro from './screens/Intro';
+import Explore from './screens/Explore'
+import LocalPoints_Vol from './screens/LocalPoints_Vol'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Intro />
+        <Explore />
       </React.Fragment>
     );
   }
