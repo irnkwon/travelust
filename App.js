2c2434f6bfe2562985e22e90a72f89d6cb5154bd
Fahad <Fahad@LAPTOP-V72H65G9>
2020-03-05 20:52:09 -0500

    Added city search functionality

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Router from './screens/Router';
+import SearchCity from './screens/SearchCity'
 
 class App extends Component {
   render() {
     return (
         <React.Fragment>
-          <Router />
+          <SearchCity />
         </React.Fragment>
     );
   }
